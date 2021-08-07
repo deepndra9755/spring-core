a3d86f4236fae2f53595970b3ea75b8b8fa989b5
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 13:14:55 -0700

    Application context preIntatation of spring bean Demo proj

---
+++ ApplicationContextPreInstatationDemo/src/main/java/com/custumer/client/Customer1.java
@@ -0,0 +1,27 @@
+package com.custumer.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.online.platform.FlipcartOn;
+
+public class Customer1 {
+
+	public static void main(String[] args) {
+ 		// TODO Auto-generated method stub
+
+   ApplicationContext bro=null;
+   bro=new FileSystemXmlApplicationContext("src/main/java/com/spr/cfg/applicationContext.xml");
+   
+       
+//      FlipcartOn op=bro.getBean("flct",FlipcartOn.class);
+//       float f[]= {34,555,333,22};
+//       System.out.println(op.shoping(f));
+        
+	}
+
+}
---
+++ ApplicationContextPreInstatationDemo/src/main/java/com/online/platform/FlipcartOn.java
@@ -0,0 +1,33 @@
+package com.online.platform;
+
+import java.util.Random;
+
+import com.online.couriour.PostService;
+
+public class FlipcartOn implements Platform {
+ 
+	 PostService poc;
+	 
+	public FlipcartOn(PostService poc) {
+		this.poc = poc;
+		System.out.println("flipcart cons");
+	}
+
+	@Override
+	public String shoping(float[] f) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+	    
+	    Random rm=new Random();
+	    int id=rm.nextInt();
+	    float count=0;
+	   for(float h:f)
+	   {
+		count=h+count;   
+	   }
+	   
+	    return "this is your bill "+count +" and this is delivery type "+poc.delivery()+" this is your id "+id+"";
+		
+	}
+
+}

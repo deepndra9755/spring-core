65fab47eddf2bb65b996a18d529938f7019f895c
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 13:19:46 -0700

    Autowiring by Constructor proj 22

---
+++ AutoWiringByConstructorProj22/src/main/java/com/online/couriour/DTDCservice.java
@@ -0,0 +1,15 @@
+package com.online.couriour;
+
+public class DTDCservice implements PostService {
+
+	public DTDCservice()
+	{
+	System.out.println("DTDC 0  param constructor");	
+	}
+	@Override	
+	public String delivery() {
+		// TODO Auto-generated method stub
+		return " welcome to DTDC post service ";
+	}
+
+}

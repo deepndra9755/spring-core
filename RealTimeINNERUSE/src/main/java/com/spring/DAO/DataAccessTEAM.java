3d20c7a0e939941316537525a44bd6503ec7069b
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 15:50:46 -0700

    Inner use

---
+++ RealTimeINNERUSE/src/main/java/com/spring/DAO/DataAccessTEAM.java
@@ -0,0 +1,8 @@
+package com.spring.DAO;
+
+import com.spring.BO.BOIMPL;
+
+public interface DataAccessTEAM {
+	public int insert(BOIMPL boo)throws Exception;
+
+}
---
+++ RealTimeINNERUSE/src/main/java/com/spring/controler/Monitoring.java
@@ -0,0 +1,7 @@
+package com.spring.controler;
+
+import com.spring.VO.VoIMPL;
+
+public interface Monitoring {
+	public void calculate(VoIMPL op)throws Exception;
+}

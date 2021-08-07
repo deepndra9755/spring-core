81f9d13a6fa716717d1b9c52f89c5e008cc88365
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 15:48:08 -0700

    Real time sallary cal 2

---
+++ RealTimeDICalculateSallary2/src/main/java/com/spring/VO/VoIMPL.java
@@ -0,0 +1,57 @@
+package com.spring.VO;
+
+public class VoIMPL {
+	String name,last,post,sallary,govtTax,Treavel;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getSallary() {
+		return sallary;
+	}
+
+	public void setSallary(String sallary) {
+		this.sallary = sallary;
+	}
+
+	public String getGovtTax() {
+		return govtTax;
+	}
+
+	public void setGovtTax(String govtTax) {
+		this.govtTax = govtTax;
+	}
+
+	public String getTreavel() {
+		return Treavel;
+	}
+
+	public void setTreavel(String treavel) {
+		Treavel = treavel;
+	}
+
+	
+	
+
+}
